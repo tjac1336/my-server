d5f2e148ebac6836ddd59874bf62c7524f6c763d
tjac1336 <38540104+tjac1336@users.noreply.github.com>
2019-10-16 18:51:33 -0500

    Created my first server

---
+++ server.js
@@ -0,0 +1,23 @@
+//stolen from slides. slide 20, module 4
+
+const http = require('http')  // require built-in Node.js http package
+
+// Use hosting values if available, otherwise default 
+const hostname = process.env.hostname || '0.0.0.0' // allow remote access
+const port = process.env.PORT || 3002
+
+// define our server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('Hello...\n')
+  res.write('Hello...\n')
+  res.end('Hello World! This is the first server I\'ve ever made!\n')
+})
+
+// start listening
+// use the server console to tell user where to find the server
+// use backticks for template literals with embedded expressions
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`)
+})
